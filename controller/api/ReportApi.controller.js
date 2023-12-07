89ca2c45c18fe355d6ea9aa26981cd308ff65917
Amit Mondal <amit.ndk3@gmail.com>
2023-12-07 16:18:43 +0530

    add operator wise report

--- controller/api/ReportApi.controller.js
+++ controller/api/ReportApi.controller.js
@@ -89,6 +89,34 @@ const shift_wise = async(req, res) => {
 }
 
 
+const operator_wise = async(req, res) => {
+    try{
+        const schema = Joi.object({
+            from_date: Joi.string().required(),
+            to_date: Joi.string().required()
+        });
+        const { error, value } = schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = {};
+            error.details.forEach(detail => {
+                errors[detail.context.key] = detail.message;
+            });
+            return res.json(sendErrorResponce(null,errors));
+        }
+
+
+        const userData = req.user;
+        const tablename=`td_vehicle_in a,td_receipt b,md_vehicle c, md_user d, md_operator e `,
+        where=`b.vehicle_in_id=a.vehicle_in_id AND c.vehicle_id=a.vehicle_id AND d.id=a.user_id_in AND e.user_id=d.user_id AND a.customer_id=${userData.customer_id} AND date(a.created_at) BETWEEN '${value.from_date}' AND '${value.to_date}' `,
+        orderby=`GROUP BY e.user_id`;
+        var data=await db_Select('a.*,SUM(b.paid_amt) AS paid_amt,c.*,e.*',tablename,where,orderby)
+        console.log(data)
+        res.json(sendOkResponce(data,null));
+    }catch(err){
+        res.json(sendErrorResponce(err));
+    }
+}
+
 
 const unbilled_report = async(req, res) => {
     try{
@@ -117,4 +145,4 @@ const unbilled_report = async(req, res) => {
     }
 }
 
-module.exports = { vehicle_wise,shift_wise,detail_report,unbilled_report };
+module.exports = { vehicle_wise,shift_wise,detail_report,operator_wise, unbilled_report };
