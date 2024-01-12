e792f2db1b9784e27276af440f3e1d3456fca9a8
Amit Mondal <amit.ndk3@gmail.com>
2024-01-12 16:29:17 +0530

    add adv amt

--- controller/api/ReportApi.controller.js
+++ controller/api/ReportApi.controller.js
@@ -136,13 +136,13 @@ const operator_wise = async(req, res) => {
 
         const userData = req.user;
 
-        var select = `b.device_id mc_srl_no_out, d.vehicle_name vehicleType, COUNT(b.receipt_no) tot_vehi, SUM(c.paid_amt) tot_amt, f.operator_name opratorName`, 
+        var select = `b.device_id mc_srl_no_out, d.vehicle_name vehicleType, COUNT(b.receipt_no) tot_vehi, SUM(c.paid_amt) tot_amt, 0 adv_amt, f.operator_name opratorName`, 
         table_name = 'td_vehicle_in a, td_vehicle_out b, td_receipt c, md_vehicle d, md_user e, md_operator f', 
         whr = `a.receipt_no=b.receipt_no AND a.receipt_no=c.receipt_no AND a.vehicle_id=d.vehicle_id AND a.user_id_in=e.id AND e.id=f.operator_id AND a.car_out_flag = 'Y' AND DATE(b.date_time_out) BETWEEN '${value.from_date}' AND '${value.to_date}' AND a.customer_id = '${userData.customer_id}'`, 
         order = 'GROUP BY a.user_id_in';
         var data = await db_Select(select, table_name, whr, order)
 
-        
+        // select adv_amt
 
 
 
